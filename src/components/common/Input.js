b7c9d2c1e679c13d6b97136f1aefc0a4173e2584
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-12-08 20:59:50 -0500

    Create an Input styled-component

---
+++ src/components/common/Input.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  padding: 4px 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1em;
+  font-family: 'open Sans';
+  margin-bottom: 8px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 40px;
+`;
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,48 @@
-import { Layout } from 'components/common';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import { Layout, Input } from 'components/common';
+
+const Form = styled.form`
+  width: 100%;
+  max-width: 400px;
+  background: #fff;
+  border: 1px solid #eee;
+  padding: 16px;
+  box-sizing: border-box;
+  color: #000;
+  border-radius: 4px;
+`;
 
 const Login = () => {
-  return <Layout>Login</Layout>;
+  const [formFields, setFormFields] = useState({ username: '', password: '' });
+
+  const handleInputChange = (e) => {
+    e.persist();
+    setFormFields((state) => ({ ...state, [e.target.name]: e.target.value }));
+  };
+
+  return (
+    <Layout>
+      <h1>Login</h1>
+      <Form>
+        <Input
+          type='text'
+          name='username'
+          placeholder='Username '
+          value={formFields.username}
+          onChange={handleInputChange}
+        />
+        <Input
+          type='password'
+          name='password'
+          placeholder='Password '
+          value={formFields.password}
+          onChange={handleInputChange}
+        />
+      </Form>
+    </Layout>
+  );
 };
 
 export default Login;

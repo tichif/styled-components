11486df92f765d9491ed65d4ab498bb9846d4f74
Dalzon Charles-Hébert <charlby5@gmail.com>
2021-12-08 21:21:35 -0500

    Implement the hide-show password functionnality

--- src/components/common/PasswordInput.js
+++ src/components/common/PasswordInput.js
@@ -10,8 +10,43 @@ const PasswordInputStyle = styled(Input).attrs(() => ({
   border-bottom-right-radius: 0;
 `;
 
+const PasswordInputWrapper = styled.div`
+  display: flex;
+
+  ~ div {
+    margin-bottom: 8px;
+  }
+`;
+
+const ToggleButton = styled.div`
+  border: 1px solid #ccc;
+  height: 40px;
+  box-sizing: border-box;
+  font-size: 0.9em;
+  display: flex;
+  padding: 8px;
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background: #fff;
+  cursor: pointer;
+  font-weight: bold;
+  user-select: none;
+  color: #000;
+`;
+
 export const PasswordInput = (props) => {
   const [showPassword, setShowPassword] = useState(false);
 
-  return <PasswordInputStyle {...props} />;
+  return (
+    <>
+      <PasswordInputWrapper>
+        <PasswordInputStyle {...props} />
+        <ToggleButton onClick={() => setShowPassword((state) => !state)}>
+          {showPassword ? 'Hide ' : 'Show '}
+        </ToggleButton>
+      </PasswordInputWrapper>
+      <div>{showPassword ? props.value : ''}</div>
+    </>
+  );
 };
